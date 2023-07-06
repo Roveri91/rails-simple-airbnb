39dcbe91f7993ce9edf69d0617bec5baa6c8da12
Simone Roveri <spikeroveri91@gmail.com>
2023-07-06 21:35:22 +0900

    working on the index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "destroying all the FLATS"
+Flat.destroy_all
+
+puts "Creating 4 New Flats..."
+
+4.times do
+  Flat.create!(
+    name: Faker::Movies::StarWars.planet,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.yoda,
+    price_per_night: rand(65...150),
+    number_of_guests: rand(1...6)
+  )
+  puts "Flat create"
+end
+
+puts "DONE!"
